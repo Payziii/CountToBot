f382cafc65a0cf8f5af00e035908196e951bc6a5
Payziii <velmikhail123@gmail.com>
2024-11-01 19:36:35 +0500

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,29 @@
+// Importing required packages
+import "dotenv/config";
+import { Bot, session, Context } from "grammy";
+import {
+    type ConversationFlavor,
+    conversations,
+    createConversation,
+} from "@grammyjs/conversations";
+import { I18n, I18nFlavor } from "@grammyjs/i18n";
+
+// Creating a bot
+type MyContext = Context & ConversationFlavor & I18nFlavor;
+const bot = new Bot<MyContext>(process.env.BOT_TOKEN);
+
+// Connecting translation
+const i18n = new I18n<MyContext>({
+    defaultLocale: "en",
+    directory: "locales",
+});
+
+// Connecting dependencies
+bot.use(session({ initial: () => ({}) }));
+bot.use(i18n);
+bot.use(conversations());
+
+// Creating a start command
+bot.command("start", async (ctx) => {
+    ctx.reply(`This is test command ;)\nGood luck!`);
+});
